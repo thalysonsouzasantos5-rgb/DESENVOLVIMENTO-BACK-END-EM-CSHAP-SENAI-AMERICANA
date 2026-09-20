37709b6f7847980c724c2ca5b4aab34651032bad
thalyson Souza Santos <thalysonsouzasantos5@gmail.com>
2026-09-20 20:51:09 -0300

    Finalizado Aula01 17/07/2026

---
+++ Lógica de programação e algoritimo/aula01/ConsoleApp1/SaidaDeDados/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+int n;
+int numero = 12;
+double flutuante = 2.85;
+string palavra = "SENAI";
+char caracter = 'S';
+bool logica1 = true;
+bool logica2 = false;
+
+WriteLine(palavra);
+WriteLine($"SENAI Americana {palavra}");
+WriteLine("Curso DEV BackEND");
+Write("Argentina");
+Write("Argentina");
+ReadKey();
