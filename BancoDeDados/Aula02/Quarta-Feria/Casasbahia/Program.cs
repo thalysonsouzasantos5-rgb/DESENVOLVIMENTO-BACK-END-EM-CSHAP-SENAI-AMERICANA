826fe9b5babaa5cc57be6a79e3e8ac545b76facf
thalyson Souza Santos <thalysonsouzasantos5@gmail.com>
2026-09-20 21:05:51 -0300

    Feito CRUD Aula02 Banco de dados

---
+++ BancoDeDados/Aula02/Quarta-Feria/Casasbahia/Program.cs
@@ -0,0 +1,107 @@
+﻿using CasasBahia.Classes.Contextos;
+using CasasBahia.Classes.Entidades;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+
+ProdutoContexto contexto = new ProdutoContexto();
+
+contexto.Database.EnsureCreated();
+
+bool continuar = true;
+
+while (continuar)
+{
+    Console.WriteLine("-- Menu do almoxarifado --" +
+        "\n 1 - Consultar item no estoque" +
+        "\n 2 - Cadastrar novo produto" +
+        "\n 3 - Sair");
+    Console.Write("\nEscolha uma opção: ");
+    int op = int.Parse(Console.ReadLine());
+
+    switch (op)
+    {
+        case 1:
+            //Consulta ao item no estoque
+            Console.Write("Digite o código do produto: ");
+            int codigo = int.Parse(Console.ReadLine());
+            var codigoProduto = contexto.Produtos.FirstOrDefault(c => c.CodigoDoProduto == codigo);
+            if (codigoProduto != null)
+            {
+                codigoProduto.ExibirDados();
+            }
+            else
+            {
+                Console.WriteLine($"Código do produto não encontrado, código: {codigo}\n");
+            }
+            break;
+        case 2:
+            //Cadastro do item no estoque
+            CadastroDeItem(contexto);
+            break;
+        case 3:
+            //Deletar item no estoque
+            DeletarItem(contexto);
+            break;
+        case 4:
+            //Sair
+            continuar = false;
+            Console.WriteLine("Encerrando sistema...");
+            break;
+        default:
+            break;
+    }
+}
+
+void CadastroDeItem(ProdutoContexto produto)
+{
+    Console.WriteLine("=== Cadastro de item ===");
+    Console.Write("Digite o nome do produto: ");
+    string nome = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(nome) || string.IsNullOrWhiteSpace(nome))
+    {
+        Console.WriteLine("Não permitido cadastro de produto sem nome");
+        return;
+    }
+
+    Console.Write("Digite a quantidade de produto: ");
+    int qtd = int.Parse(Console.ReadLine());
+    if (qtd < 0)
+    {
+        Console.WriteLine("Não permitido valores negativo de itens");
+    }
+    Console.Write("Digite o custo do produto: ");
+    decimal valor = decimal.Parse(Console.ReadLine());
+    if (valor < 0)
+    {
+        Console.WriteLine("Não permitido cadastrar valores negaticos de custo de produto");
+    }
+
+    Produto produto1 = new Produto(nome, qtd, valor);
+    contexto.Produtos.Add(produto1);
+    contexto.SaveChanges();
+    produto1.ExibirDados();
+}
+
+void DeletarItem(ProdutoContexto contexto)
+{
+    Console.Write("Digite o código do produto: ");
+    int codigo = int.Parse(Console.ReadLine());
+    var codigoProduto = contexto.Produtos.FirstOrDefault(c => c.CodigoDoProduto == codigo);
+
+    if (codigoProduto != null)
+    {
+        Console.Write("Deseja realmente realizar esta operação (s/n)? ");
+        char escolha = char.Parse(Console.ReadLine().ToLower());
+        if (escolha.Equals('s'))
+        {
+            contexto.Produtos.Remove(codigoProduto);
+            contexto.SaveChanges();
+            Console.WriteLine("Operação realizado com sucesso!");
+        }
+        else
+        {
+            Console.WriteLine("Operação cancelada");
+            return;
+        }
+    }
+}
---
+++ BancoDeDados/Aula02/Quarta-Feria/Casasbahia/classes/Entidades/Produto.cs
@@ -0,0 +1,33 @@
+﻿
+namespace CasasBahia.Classes.Entidades
+{
+    internal class Produto
+    {
+
+        //Propriedades
+        public int CodigoDoProduto { get; set; }
+        public string NomeDoProduto { get; set; }
+        public int QuantidadeDeProduto { get; set; }
+        public decimal ValorDoProduto { get; set; }
+
+        //Contrutor
+        public Produto(string nomeDoProduto, int quantidadeDeProduto, decimal valorDoProduto)
+        {
+            NomeDoProduto = nomeDoProduto;
+            QuantidadeDeProduto = quantidadeDeProduto;
+            ValorDoProduto = valorDoProduto;
+        }
+
+        //Métodos
+        public void ExibirDados()
+        {
+            Console.WriteLine("-- Dados do produto --" +
+                $"\nCódigo do produto: {CodigoDoProduto}" +
+                $"\nNome do produto: {NomeDoProduto}" +
+                $"\nQuantidade de produto: {QuantidadeDeProduto}" +
+                $"\nValor do produto: {ValorDoProduto:c}");
+        }
+
+
+    }
+}
