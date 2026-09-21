561112591c5e50214d8722dd7e4c7c0d81e7a12b
thalyson Souza Santos <thalysonsouzasantos5@gmail.com>
2026-09-21 13:10:26 -0300

    Programa Para calcular terreno

---
+++ Lógica de programação e algoritimo/Aula02/Terreno/Terreno/Program.cs
@@ -0,0 +1,24 @@
+﻿//Declaração de variáveis
+
+double largura;
+double comprimento;
+double valor;
+double area;
+double preco;
+
+//Entrada de dados
+Console.Write("Digite a largura do terreno: ");
+largura = double.Parse(Console.ReadLine());
+
+Console.Write("Digite o comprimento do terreno: ");
+comprimento = double.Parse(Console.ReadLine());
+
+Console.Write("Digite o valor do metro quadrado do terreno: ");
+valor = double.Parse(Console.ReadLine());
+
+//Processamento de dados
+area = largura * comprimento;
+preco = area * valor;
+
+//Saída de dados
+Console.WriteLine($"A área do terreno é: {area:n}\nPreço do terreno: R$ {preco:n}");
